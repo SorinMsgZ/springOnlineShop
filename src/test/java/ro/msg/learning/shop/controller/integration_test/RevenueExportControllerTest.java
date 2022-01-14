23160f64ba2f3d8d6dec69d768c36f42c3692614
SorinMsgZ <sorin.zdrenghia@msg.group>
2022-01-14 11:55:15 +0200

    commit ch9 before push & creating PR

--- src/test/java/ro/msg/learning/shop/controller/integration_test/RevenueExportControllerTest.java
+++ src/test/java/ro/msg/learning/shop/controller/integration_test/RevenueExportControllerTest.java
@@ -1,32 +1,23 @@
 package ro.msg.learning.shop.controller.integration_test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SequenceWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.jayway.jsonpath.JsonPath;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.core.env.Environment;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ro.msg.learning.shop.dto.*;
-import ro.msg.learning.shop.entities.Customer;
-import ro.msg.learning.shop.entities.Location;
-import ro.msg.learning.shop.entities.Order;
-import ro.msg.learning.shop.entities.Product;
+import ro.msg.learning.shop.entities.*;
 import ro.msg.learning.shop.repositories.*;
 import ro.msg.learning.shop.services.*;
 
@@ -35,10 +26,14 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -48,7 +43,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource("classpath:test.properties")
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
-@ActiveProfiles("TestProfile3")
+@ActiveProfiles({"with-base", "TestProfile3"})
 public class RevenueExportControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -76,7 +71,13 @@ public class RevenueExportControllerTest {
     private CustomerService customerService;
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    UserRepository userRepository;
     private LocalDate localDateToTest;
+    User userTest;
+    private String activeProfile;
+    @Autowired
+    private Environment environment;
 
     public void populateDataBase() {
         CustomerDTO customerDTO = CustomerDTO.builder()
@@ -173,11 +174,33 @@ public class RevenueExportControllerTest {
         orderDetailService.create(orderDetailDTO3);
         orderDetailService.create(orderDetailDTO4);
         orderDetailService.create(orderDetailDTOOld5);
+
+        activeProfile = Arrays.toString(environment.getActiveProfiles());
+
+        Customer customer2 =   new Customer();
+        customer2.setFirstName("Cliff");
+        customer2.setLastName("Clifescu");
+        customer2.setEmailAddress("clif@clif.com");
+        customer2.setUserName("clifut");
+        customer2.setPassword("example123");
+        customer2.setId(1);
+        customerRepository.save(customer2);
+
+        userTest = new User();
+        userTest.setCustomer(customer2);
+        userTest.setUsername("cliff");
+
+        if (activeProfile.equals("[with-base, TestProfile3]")) {
+            userTest.setPassword("{bcrypt}$2a$10$c1UdwXGZf97TYBkA8ZGquuf6WZI.zl6LAmAw3tMdMKEc94ifWrKEa");
+        } else if (activeProfile.equals("[with-form, TestProfile3]")) {
+            userTest.setPassword("$2a$10$c1UdwXGZf97TYBkA8ZGquuf6WZI.zl6LAmAw3tMdMKEc94ifWrKEa");
+        }
+        userRepository.save(userTest);
     }
 
 
     @Test
-    public void testAggregateAndStoreSalesRevenues() throws Exception {
+    public void testReadRevenuesByLocalDateStringAfterAggregateAndStoreSalesRevenuesByTaskScheduler() throws Exception {
 
         populateDataBase();
 
@@ -189,33 +212,41 @@ public class RevenueExportControllerTest {
         Assert.assertEquals(30, schedulerRevenueTask.getRevenueList().get(0).getSum().intValue());
         Assert.assertEquals(5, schedulerRevenueTask.getRevenueList().get(1).getSum().intValue());
 
-//        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        String listAsStringDTO = objectMapper.writeValueAsString(schedulerRevenueTask.getRevenueList());
+
+        RevenueExportDTO revenueExportDTO1 = RevenueExportDTO.builder()
+                .locationId(schedulerRevenueTask.getRevenueList().get(0).getLocation().getId())
+                .localDateString(schedulerRevenueTask.getRevenueList().get(0).getLocalDate().toString())
+                .sumInt(schedulerRevenueTask.getRevenueList().get(0).getSum().intValue())
+                .build();
+        RevenueExportDTO revenueExportDTO2 = RevenueExportDTO.builder()
+                .locationId(schedulerRevenueTask.getRevenueList().get(1).getLocation().getId())
+                .localDateString(schedulerRevenueTask.getRevenueList().get(1).getLocalDate().toString())
+                .sumInt(schedulerRevenueTask.getRevenueList().get(1).getSum().intValue())
+                .build();
+
+        List<RevenueExportDTO> revenueExportDTOList = new ArrayList<>();
+        revenueExportDTOList.add(revenueExportDTO1);
+        revenueExportDTOList.add(revenueExportDTO2);
+
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema schema = mapper.schemaFor(RevenueExportDTO.class);
+        StringWriter strW = new StringWriter();
+        SequenceWriter seqW = mapper.writer(schema).writeValues(strW);
+        seqW.write(revenueExportDTOList);
+        seqW.close();
 
         String headerOfCsvText = "location,localDate,sum" + "\n";
-//        String expectedCsvText = headerOfCsvText + strW.toString();
+        String expectedTwoRevenueExportDTOAsCsvText = headerOfCsvText + strW.toString();
 
-        MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/api/revenues/export/csv/" + localDateToTest.toString()))
+        mvc.perform(MockMvcRequestBuilders.get("/api/revenues/export/csv/" + localDateToTest.toString())
+                .with(httpBasic("cliff", "example123")))
+                .andExpect(authenticated().withUsername("cliff"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/csv"))
                 .andExpect(content()
-                        .string(containsString(headerOfCsvText)))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.[0]shippedFrom.id").value(expectedOrderLocationId1))
+                        .string(containsString(expectedTwoRevenueExportDTOAsCsvText)))
                 .andReturn();
 
-
-//        int idOrderOne = JsonPath.read(result.getResponse().getContentAsString(), "$.[0]shippedFrom.id");
-
-       /* MvcResult result = mvc.perform(MockMvcRequestBuilders.get("/api/revenues/export/json/" + localDateToTest.toString()))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType("text/csv"))
-//                .andExpect(MockMvcResultMatchers.jsonPath("$.[0]shippedFrom.id").value(expectedOrderLocationId1))
-                .andReturn();
-//        int idOrderOne = JsonPath.read(result.getResponse().getContentAsString(), "$.[0]shippedFrom.id");*/
     }
 }

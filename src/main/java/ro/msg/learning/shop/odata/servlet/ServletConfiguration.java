22f1fc97d1e3533a0333a02e20a9d7194d4b8c86
SorinMsgZ <sorin.zdrenghia@msg.group>
2022-02-09 16:30:27 +0200

    commit initial implementation of exposing entities through a JPA-based OData service

---
+++ src/main/java/ro/msg/learning/shop/odata/servlet/ServletConfiguration.java
@@ -0,0 +1,13 @@
+package ro.msg.learning.shop.odata.servlet;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServletConfiguration {
+    @Bean
+    public ServletRegistrationBean odataServlet(ExampleServlet servlet) {
+        return new ServletRegistrationBean(servlet, "/odata/*");
+    }
+}

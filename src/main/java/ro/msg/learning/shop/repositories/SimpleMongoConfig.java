980a9d106c0574f7b35dad2a80f754e345505596
SorinMsgZ <sorin.zdrenghia@msg.group>
2022-02-18 15:39:16 +0200

    commit implementation for optional chapter 6 RabbitMQ

--- src/main/java/ro/msg/learning/shop/repositories/SimpleMongoConfig.java
+++ src/main/java/ro/msg/learning/shop/repositories/SimpleMongoConfig.java
@@ -7,9 +7,10 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
+@Profile("mongodb")
 @Configuration
 @EnableMongoRepositories(basePackages = "ro.msg.learning.shop.repositories")
 
